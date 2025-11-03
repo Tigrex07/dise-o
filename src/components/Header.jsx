121903cad0b6d42783c3b8c96b8de8f952686f9e
Tigrex07 <136181670+Tigrex07@users.noreply.github.com>
2025-11-03 12:15:15 -0700

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+
+function Header() {
+
+    return (
+    <>
+        <h1>Machine Shop</h1>
+
+        <nav>
+
+            <ul>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Requerimientos</li>
+                <li>Acerca de</li>
+            </ul>
+        </nav>
+    </> )
+}
+
+export default Header
